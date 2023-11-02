678f0c5fb46381fb2db54561e49337c5da9df8ab
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-11-02 08:06:42 +0545

    feat: :tada: Starting to build a blog app using Next Js

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: [],
+  },
+  darkMode: "class",
+  theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  theme: {
+    extend: {
+      colors: {
+        dark: "#232A3C",
+        medium: "#293245",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
