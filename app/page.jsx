edadaded6de001f1d0182d3263b702cef4c2a2b7
Sristhi lamichhane <srishtilamichhane83@gmail.com>
2023-11-05 01:16:11 +0545

    feat: :sparkles: adding the newsletter on home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import LatestPost from "../components/LatestPost";
+import NewsLetter from '../components/NewsLetter';
 
 const Home = () => {
   return(
     <>
     <Hero/>
     <LatestPost/>
+    <NewsLetter/>
     </>
   );
 };
